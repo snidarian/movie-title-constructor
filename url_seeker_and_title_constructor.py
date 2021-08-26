f72a91aa3bd3b56a66d2b6f0f93bc73f7fa3f0a9
snidarian <cephalopod31956@gmail.com>
2021-08-26 17:19:16 -0400

    continuous development

--- url_seeker_and_title_constructor.py
+++ url_seeker_and_title_constructor.py
@@ -5,9 +5,7 @@
 # 2. reach the imdb pages of the .csv rows with advanced search, take the urls and create a "url_list.csv" file in the CWD
 # 3. Create a titles_list.csv file delimited by spaces in the CWD using the urls in url_list.csv.
 
-
-import argparse
-
+# IMPORTS
 from selenium import webdriver
 # Service class fix taken from https://dev.to/t00m/testing-selenium-4-0-0a5-pre-release-with-python-42h7
 from selenium.webdriver.firefox.service import Service
@@ -20,6 +18,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import time
 # HTML parsing
 from bs4 import BeautifulSoup as bs
 # CSV library for writing data into and using data from .csv files
@@ -27,30 +26,90 @@ import csv
 # colored terminal output for highlighting certain pieces of information useful to the user
 
 
+# SETTING UP SELENIUM ON FIREFOX BROWSER
 # setting up the Firefox browser with global scope
 options = Options()
 
 # set this to the path of your firefox profile file in the .mozilla folder in home dir
-options.profile = '/home/cn1d4r14n/.mozilla/firefox/ct8fbiwt.default'
+options.profile = '/home/cn1d4r14n/.mozilla/firefox/t8oqdk41.default-esr'
 # allows you to run the browser as a background process I believe
 options.headless = False
 
-# link this to a web browser driver
+# link this to a web browser driver - here I'm using the geckodriver web driver file I downloaded online
 service = Service('/home/cn1d4r14n/Documents/geckodriver')
 
 driver = webdriver.Firefox(options=options, service=service)
 
 
+# Takes title string and year and returns URL string of first result
+def advanced_search_to_grab_url(title_string, year) -> str:
+    print(f"{title_string} ({year})")
 
+    driver.get('https://www.imdb.com/search/title/?ref_=kw_asr_tt')
+    search_box = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div[3]/form/div/div[1]/div[2]/input")
+    from_year_box = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div[3]/form/div/div[3]/div[2]/input[1]")
+    to_year_box = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div[3]/form/div/div[3]/div[2]/input[2]")
+    # enter details into the three boxes
+    search_box.send_keys(title_string)
+    from_year_box.send_keys(year)
+    to_year_box.send_keys(year)
+    # Press ENTER
+    to_year_box.send_keys(Keys.RETURN)
+    time.sleep(3)
+    # click on the top search result and navigate to that page.
+    first_search_result = driver.find_element_by_class_name("lister-item-header").find_element_by_tag_name('a')
+    # click the link and navigate to the movie-listing page
+    first_search_result.click()
+    # return the recovered URL
+    return driver.current_url
+
+
+# Use search_titles.csv file to create a url_list.csv file
+def search_titles_and_create_url_list_csv() -> None:
+    # Open the search_titles.csv file (created by user prior to search)
+    with open('search_titles.csv') as csv_search_data:
+            csv_reader = csv.reader(csv_search_data, delimiter=',')
+            url_list = []
+            for row in csv_reader:
+                url = advanced_search_to_grab_url(row[0], row[1])
+                url_list.append(url)
+            print(url_list)
+    # create url_list.csv file
+    with open('url_list.csv', mode='w') as url_data_file:
+        url_list_writer = csv.writer(url_data_file, delimiter=',')
+        for url_item in url_list:
+            # important that the row item(s) be included as list items or else it will make every single character a different row-field
+            url_list_writer.writerow([url_item])
+
+def scrape_movie_data_with_urls_csv() -> None:
+    # use url_list.csv to reach each page
+    with open('url_list.csv') as url_data_file:
+        csv_reader = csv.reader(url_data_file, delimiter='\n')
+        # for each url: .get() method and then scrape [Title, year, three top actors, genres]
+        movie_data_list = []
+        for url in csv_reader:
+            print(f"Getting url: {url}")
+            driver.get(f"{url[0]}")
+            movie_title = driver.find_element_by_xpath("/html/body/div[2]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/h1").text
+            movie_year = driver.find_element_by_xpath("/html/body/div[2]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/div[2]/ul/li[1]/a").text
+            print(movie_title)
+            print(movie_year)
+            # make list item
+            
+            
+
+            
+
+def contruct_formatted_titles_and_save_to_csv() -> None:
+    pass
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Produces formatted movie title list from string list of titles')
-    args = parser.add_argument("title_search_string", help="movie title and year string", nargs="*", type=str)
-    args = parser.parse_args()
-
-    for item in args.title_search_string:
-        print(item)
+    # use search_titles.csv to create url_list.csv
+    search_titles_and_create_url_list_csv()
+    # use url_list.csv to reach each movie page and scrape relevant data
+    scrape_movie_data_with_urls_csv()
+              
 
 
     
