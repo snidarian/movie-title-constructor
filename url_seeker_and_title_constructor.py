8806579529b02c007bab811c36b05adacfd587b0
snidarian <cephalopod31956@gmail.com>
2021-08-26 11:40:32 -0400

    combined both functional files into one

---
+++ url_seeker_and_title_constructor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# This program will take a list of string arguments ("[MOVIE TITLE] [YEAR]"), parse them using argparse, reach the imdb pages of the first listing for the query, take the urls and list
+
+
+
+
+
+
+
+
+
+
